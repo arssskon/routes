5d55a362d5a91a8c4269dfef66f1781f74cdeda1
arssskon <arskon_sb@mai.ru>
2023-10-22 17:17:35 +0500

    swag

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PageViewSet } from './components/PageViewSet';
+import { PageCreateSet } from './components/PageCreateSet';
+
+import { PageSelectSet } from './components/PageSelectSet';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageSelectSet/>} />
+        <Route path="/set" element={<PageViewSet/>} />
+        <Route path="/admim" element={<PageCreateSet/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/PageCreateSet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Header} from './Header'
+
+export function PageCreateSet() {
+    return <div>
+        <Header />
+        <h2>Создание сета карточке</h2>
+    </div>
+}
